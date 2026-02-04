fdd92f3874e3fd18455fccda771ce78218dc30fe
dibardyk <dianabardyk0@gmail.com>
2026-02-04 12:30:39 +0100

    Add files via upload

---
+++ main/dashboard/pie/create_pie.py
@@ -0,0 +1,11 @@
+from pie_val_lab import pie_values_labels
+import matplotlip.figure as Figure
+
+def create_pie(subject_hours):
+    fig = Figure()
+    ax = fig.add_subplot(111)
+
+    values, labels = pie_values_labels(subject_hours)
+    ax.pie(values, labels = labels, autopct = '%1.1f%%', startangle=90)
+
+    return fig
